9023ffdf069ff5d29b5f1d26b01b3aeed34db1fa
HenriLMU <50552831+HenriLMU@users.noreply.github.com>
2020-04-06 12:47:01 +0200

    init nnet

---
+++ inst/paramtest/test_paramtest_classif.nnet.R
@@ -0,0 +1,34 @@
+library(mlr3learners.rweka)
+
+test_that("classif.JRip", {
+  learner = lrn("classif.JRip")
+  fun = RWeka::JRip
+    exclude = c(
+      "formula", # handled via mlr3
+      "data", # handled via mlr3
+      "control" # handled to RWeka::Weka_Control
+    )
+    ParamTest = run_paramtest(learner, fun, exclude)
+    expect_true(ParamTest, info = paste0(
+      "
+Missing parameters:
+",
+      paste0("- '", ParamTest$missing, "'", collapse = "
+")))
+})
+
+test_that("Weka_control JRip", {
+  learner = lrn("classif.JRip")
+  fun = RWeka::Weka_control
+  exclude = c(
+    character(0L)
+  )
+  
+  ParamTest = run_paramtest(learner, fun, exclude)
+  expect_true(ParamTest, info = paste0(
+    "
+Missing parameters:
+",
+    paste0("- '", ParamTest$missing, "'", collapse = "
+")))
+})
